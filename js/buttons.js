26e85647e4393cefa5ddfad952d993f64edc0ff6
T-RexZ <victor.kristiansen@gmail.com>
2016-02-16 10:17:30 +0100

    Create buttons.js

---
+++ js/buttons.js
@@ -0,0 +1,17 @@
+function show_login() {
+
+
+	document.getElementById('login-form').classList.toggle('visible');
+
+
+
+}
+
+function show_register() {
+
+
+	document.getElementById('register-form').classList.toggle('visible');
+
+
+
+}
