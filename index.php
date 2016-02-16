dd2412197dd04626bb4b85cc796c22a793fc89b1
Nakarda <jimmyp2650@hotmail.com>
2016-02-16 13:24:45 +0100

    up
    up

--- index.php
+++ index.php
@@ -1,39 +1,74 @@
-
 <!DOCTYPE html>
 <html>
 <head>
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Login</title>
+	<link rel="stylesheet" type="text/css" href="css/normalize.css">
 	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 	<body class="container max-40-vw">
 	<div class="row">
-		<button class="btn col-sm-12 max-40-vw" onclick="show_login()">
+		<button class="btn" onclick="show_login()">
 			I already have a login
 		</button>
 			
 		
 
-		<form id="login-form" action="login.php" class="col-sm-12 non-visible max-40-vw row" method="post">
+		<form id="login-form" action="login.php" class="non-visible row" method="post">
 
-			<input class="col-sm-12" type="text" placeholder="email" name="login_email"></input>
+			<input type="text" placeholder="email" name="login_email"></input>
 			
-			<input class="col-sm-12" type="password" placeholder="password" name="login_pw"></input>
+			<br>
+
+			<input type="password" placeholder="password" name="login_pw"></input>
 			
-			
+			<br>
 			
 			<input class="btn" type="submit" name="login_submit"></input>
+
+			<br>
+
+
 		</form>
 
 
-		<button class="btn  col-sm-12 max-40-vw" onclick="show_register()">
+		<button class="btn" onclick="show_register()">
 			I dont have a login
 		</button>
 
-		<form id="register-form" action="rig.php" class="col-sm-12 non-visible container">
+		<form id="register-form" action="reg.php" class="non-visible row">
 			
+			<input type="text" placeholder="email" name="reg_email"></input>
+			
+			<br>
+
+			<input type="password" placeholder="password" name="reg_pw"></input>
+			
+			<br>
+			
+			<input type"password" placeholder="repeat password" name="reg_rep_pw"></input>
+
+			<br>
+
+			<input type="text" placeholder="phone number" name="reg_phone"></input>
+
+			<br>
+
+			<input class="btn" type="submit" name="reg_submit"></input>
+
+			<br>
+
+
+
 
 		</form>
+
+		
+
+		<a href="about.php">What is PokeDex?</a>
 
 	</div>
 		<script src="js/bootstrap.min.js"></script>
