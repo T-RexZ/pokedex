56b31128a7a90b6368273724b7c75fc807c92f83
victor kristiansen <victor.kristiansen@gmail.com>
2016-02-17 08:34:53 +0100

    tilføjede farve på chrome for android

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 	<meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="theme-color" content="#2D78BB">
 	<title>Login</title>
 	<link rel="stylesheet" type="text/css" href="css/normalize.css">
 	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
